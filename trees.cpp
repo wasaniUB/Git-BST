68dd59dc3a19ffd553625d5ec1b42b7e14fb8ee6
Wasani Castillo <2024160540@ub.edu.bz>
2025-10-05 08:55:40 -0600

    Added struct

--- trees.cpp
+++ trees.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 using namespace std;
 
+struct BSTNode {
+    int data;
+    BSTNode* left;
+    BSTNode* right;
+
+     BSTNode(int value) {
+        data = value;
+        left = nullptr;
+        right = nullptr;
+     }
+};
 
 int main() {
 
