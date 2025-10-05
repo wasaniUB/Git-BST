84a135ebd25d6be0ff1a632c7c5f2800429e894f
Wasani Castillo <2024160540@ub.edu.bz>
2025-10-05 09:12:49 -0600

    Added insertIterative fxn

--- trees.cpp
+++ trees.cpp
@@ -14,6 +14,7 @@ struct BSTNode {
 };
 
 void insertRecursive(BSTNode*& root, int value);
+void insertIterative(BSTNode*& root, int value);
 
 int main() {
 
@@ -34,4 +35,39 @@ void insertRecursive(BSTNode*& root, int value) {
      else if(value > root->data) {
         insertRecursive(root->right, value);
      }
+
+}
+
+void insertIterative(BSTNode*& root, int value) {
+    if(!root) {
+        root = new BSTNode(value);
+        return;
+    }
+      
+     BSTNode* cur = root;
+     BSTNode* parent = nullptr;
+
+      while(cur) {
+        parent = cur;
+
+         if(value < cur->data) {
+            cur = cur->left;
+         }
+
+          else if(value > cur->data) {
+            cur = cur->right;
+          }
+
+          else {
+            return;
+        }
+      }
+
+      if(value < parent->data) {
+        parent->left = new BSTNode(value);
+      }
+
+      else{
+        parent->right = new BSTNode(value);
+      }
 }
