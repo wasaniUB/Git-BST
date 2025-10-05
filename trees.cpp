58329e3ca32c701529f078210a33591680fca37c
Wasani Castillo <2024160540@ub.edu.bz>
2025-10-05 09:20:53 -0600

    Added searchRecursive fxn

--- trees.cpp
+++ trees.cpp
@@ -15,6 +15,7 @@ struct BSTNode {
 
 void insertRecursive(BSTNode*& root, int value);
 void insertIterative(BSTNode*& root, int value);
+bool searchRecursive(BSTNode*& root, int key);
 
 int main() {
 
@@ -70,4 +71,24 @@ void insertIterative(BSTNode*& root, int value) {
       else{
         parent->right = new BSTNode(value);
       }
+
+}
+
+bool searchRecursive(BSTNode*& root, int key) {
+    if(!root) {
+        return false;
+    }
+
+    if(root->data == key) {
+        return true;
+    }
+
+    if(key < root->data) {
+        return searchRecursive(root->left, key);
+    }
+
+    else{
+        return searchRecursive(root->right, key);
+    }
+
 }
